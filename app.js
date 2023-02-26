a017f05b766d555e4d1af694d686aea80b009408
Linh Le <fenix.lin@yandex.ru>
2023-02-27 02:47:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const recipes = [
+    {
+      id: 1,
+      name: "James Hoffmann",
+      title: "World Barista Champion 2007",
+      img: "https://images.squarespace-cdn.com/content/v1/5d39c0ab87164d000167b2e2/1600945711706-RICUCBD9HQ9PEWESXJ8E/_DSC4757.jpg?format=1000w",
+      video:
+        "https://www.youtube.com/watch?v=mazSs3E_cwY",
+      text:
+        "Use water that is pretty much right off the boil. Use a grind that is slightly coarser than espresso, but finer than drip (James describes it as between powdered sugar and salt).",
+    },
+    {
+      id: 2,
+      name: "Tim Wendelboe",
+      title: "Norwegian barista champion 2004",
+      img: "https://kinfolkmagprod.wpenginepowered.com/wp-content/uploads/2018/03/Skandia_Kinfolk_TimWendelboe_Oslo_01.jpg",
+      video:
+        "https://youtu.be/wGZ3VdCJvu0",
+      text:
+        "This AeroPress recipe is exactly what they use in the Tim Wendelboe cafe in Oslo, Norway.",
+    },
+    {
+      id: 3,
+      name: "Raphael Barbedouce",
+      title: "Head Barista at Maxi Coffee Shop",
+      img: "https://www.maxicoffee.com/fstrz/r/s/www.maxicoffee.com/images/shop-store/LaTeste/IMAGE,P202.jpg.pagespeed.ce.33OktXMNDa.jpg?frz-v=80",
+      video:
+        "https://youtu.be/lwOgQEELcrI",
+      text:
+        "This is Raphael's personal AeroPress recipe for most mornings' coffee routine. He uses a double filter for this recipe",
+    },
+    {
+        id: 4,
+        name: "Tetsu Kasuya",
+        title: "World Brewers Cup 2016 Champion",
+        img: "https://sprudge.com/wp-content/uploads/2017/08/Tetsu-Kasuya_02-1170x779.jpg",
+        video:
+          "https://www.instagram.com/tv/CORkSoaiNoO/?igshid=1xiq2ov3mpxo2",
+        text:
+          "This AeroPress recipe is also a great option to make a larger volume of coffee.",
+      },
+      {
+        id: 5,
+        name: "Ralf Rueller",
+        title: "Best Specialty Coffee Roaster 2019",
+        img: "https://www.comunicaffe.com/wp-content/uploads/2022/02/Ralf-Rueller.jpeg",
+        video:
+          "https://youtu.be/RO2jjz_1fBI",
+        text:
+          "This is the AeroPress recipe of The Barn Founder, Ralf Rueller. It aims for a juicy cup that has a creamy mouth feeling.",
+      },
+  ];
