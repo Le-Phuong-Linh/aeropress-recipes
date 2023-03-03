f34f4a57e0991c45b13665f9baf5c474a059ac46
Linh Le <fenix.lin@yandex.ru>
2023-03-03 22:03:15 +0300

    added formatted pictures

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const recipes = [
       id: 1,
       name: "James Hoffmann",
       title: "World Barista Champion 2007",
-      img: "https://images.squarespace-cdn.com/content/v1/5d39c0ab87164d000167b2e2/1600945711706-RICUCBD9HQ9PEWESXJ8E/_DSC4757.jpg?format=1000w",
+      img: "img/hoffmann.png",
       video:
         "https://www.youtube.com/watch?v=mazSs3E_cwY",
       text:
@@ -13,7 +13,7 @@ const recipes = [
       id: 2,
       name: "Tim Wendelboe",
       title: "Norwegian barista champion 2004",
-      img: "https://kinfolkmagprod.wpenginepowered.com/wp-content/uploads/2018/03/Skandia_Kinfolk_TimWendelboe_Oslo_01.jpg",
+      img: "img/wendelboe.png",
       video:
         "https://youtu.be/wGZ3VdCJvu0",
       text:
@@ -23,7 +23,7 @@ const recipes = [
       id: 3,
       name: "Raphael Barbedouce",
       title: "Head Barista at Maxi Coffee Shop",
-      img: "https://www.maxicoffee.com/fstrz/r/s/www.maxicoffee.com/images/shop-store/LaTeste/IMAGE,P202.jpg.pagespeed.ce.33OktXMNDa.jpg?frz-v=80",
+      img: "img/barbedouce.jpg",
       video:
         "https://youtu.be/lwOgQEELcrI",
       text:
@@ -33,7 +33,7 @@ const recipes = [
         id: 4,
         name: "Tetsu Kasuya",
         title: "World Brewers Cup 2016 Champion",
-        img: "https://sprudge.com/wp-content/uploads/2017/08/Tetsu-Kasuya_02-1170x779.jpg",
+        img: "img/kasuya.png",
         video:
           "https://www.instagram.com/tv/CORkSoaiNoO/?igshid=1xiq2ov3mpxo2",
         text:
@@ -43,10 +43,35 @@ const recipes = [
         id: 5,
         name: "Ralf Rueller",
         title: "Best Specialty Coffee Roaster 2019",
-        img: "https://www.comunicaffe.com/wp-content/uploads/2022/02/Ralf-Rueller.jpeg",
+        img: "img/rueller.png",
         video:
           "https://youtu.be/RO2jjz_1fBI",
         text:
           "This is the AeroPress recipe of The Barn Founder, Ralf Rueller. It aims for a juicy cup that has a creamy mouth feeling.",
       },
   ];
+
+  const baristaName = document.getElementById('barista-name');
+  const baristaTitle = document.getElementById('barista-title');
+  const baristaPhoto = document.getElementById('barista-photo');
+  const recipeVideo = document.getElementById('video');
+  const recipeDescription = document.getElementById('video_description');
+  
+  const randomizeBtn = document.querySelectorAll('randomize');
+
+  let recipeIndex = 0;
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    showRecipe();
+  })
+
+  function showRecipe () {
+    let randomRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+    baristaName.textContent = randomRecipe.name;
+    baristaTitle.textContent = randomRecipe.title;
+    baristaPhoto.src = randomRecipe.img;
+    recipeVideo.src = randomRecipe.video;
+    recipeDescription.textContent = randomRecipe.text
+  }
+
+  randomizeBtn.addEventListener('click', showRecipe());
