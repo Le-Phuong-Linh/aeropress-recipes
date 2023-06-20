798a1b50842491e9b3896301fb5986d6a60784ca
Linh Le <fenix.lin@yandex.ru>
2023-06-20 04:52:44 +0300

    created a separate file for recipes

---
+++ recipes.js
@@ -0,0 +1,62 @@
+const recipes = [
+    {
+      id: 1,
+      name: "James Hoffmann",
+      title: "World Barista Champion 2007",
+      img: "img/hoffmann.png",
+      video:
+        "https://www.youtube.com/embed/mazSs3E_cwY",
+      text:
+        "Use water that is pretty much right off the boil. Use a grind that is slightly coarser than espresso, but finer than drip (James describes it as between powdered sugar and salt).",
+    },
+    {
+      id: 2,
+      name: "Tim Wendelboe",
+      title: "Norwegian barista champion 2004",
+      img: "img/wendelboe.png",
+      video:
+        "https://www.youtube.com/embed/wGZ3VdCJvu0",
+      text:
+        "This AeroPress recipe is exactly what they use in the Tim Wendelboe cafe in Oslo, Norway.",
+    },
+    {
+        id: 3,
+        name: "Anthony Douglas",
+        title: "World Barista Champion 2022",
+        img: "img/douglas.jpg",
+        video:
+          "https://www.youtube.com/embed/cittrtT7SNA",
+        text:
+          "Anthony Douglas shares a modified—and simplified—version of his signature drink from the 2022 World Barista Championship. It is called \"The Familiar Charm\" ",
+      },
+    {
+        id: 4,
+        name: "Tetsu Kasuya",
+        title: "World Brewers Cup 2016 Champion",
+        img: "img/kasuya.png",
+        video:
+          "https://www.youtube.com/embed/HDmhY5adR3s",
+        text:
+          "\"This is a recipe that I introduced on Instagram and is used by people all over the world every year.I would like you to skip the difficult things and enjoy it like a trial and error game like how to place the ice or a puzzle! If you are concerned about uneven extraction, crush the ice and try crushed ice.\"",
+      },
+      {
+        id: 5,
+        name: "Ralf Rueller",
+        title: "Best Specialty Coffee Roaster 2019",
+        img: "img/rueller.png",
+        video:
+          "https://www.youtube.com/embed/RO2jjz_1fBI",
+        text:
+          "This is the AeroPress recipe of The Barn Founder, Ralf Rueller. It aims for a juicy cup that has a creamy mouth feeling.",
+      },
+      {
+        id: 6,
+        name: "Agnieszka Rojewska",
+        title: "World Barista Champion 2018",
+        img: "img/agnieszka.jpg",
+        video:
+          "https://www.youtube.com/embed/MxvdmG9CiDE",
+        text:
+          "This is one of the recipes from her award-winning presentation.",
+      }
+  ];
